ac69a112ab1180b42093a285f1aa08fabb4647c6
Alejandro <112018586+4x3l22@users.noreply.github.com>
2024-05-18 23:20:16 -0500

    primera carga de proyecto

--- componentes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="py-5 fondo">
-        <div class="container-fluid">
-            <p class="text_footer">Dirección: </p>
-        </div>
-</footer>
--- librerias/acceso.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include('classAcceso.php'); 
-    include('conexion.php');
-
-    
-?>
